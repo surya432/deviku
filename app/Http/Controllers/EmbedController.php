fee645b6b1374441872d26e45283cb3eb624b480
surya432 <hadisurya295@gmail.com>
2019-01-05 08:34:08 +0700

    Update EmbedController.php

--- app/Http/Controllers/EmbedController.php
+++ app/Http/Controllers/EmbedController.php
@@ -95,10 +95,10 @@ class EmbedController extends Controller
                         return abort(404);
                     };
                     //return file_get_contents("http://db.nontonindrama.com/Player-Script/json.php?url=https://drive.google.com/open?id=".$copyID);
-                    return file_get_contents("https://player.dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
+                    return file_get_contents("https://dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
                 }else{
                     //return file_get_contents("http://db.nontonindrama.com/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
-                    return file_get_contents("https://player.dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
+                    return file_get_contents("https://dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
                 }
                 break;
             case 'gd720':
@@ -109,9 +109,9 @@ class EmbedController extends Controller
                     if(is_null($copyID)){ 
                         return abort(404);
                     };
-                    return file_get_contents("https://player.dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
+                    return file_get_contents("https://dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
                 }else{
-                    return file_get_contents("https://player.dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
+                    return file_get_contents("https://dldramaid.xyz/Player-Script/json.php?url=https://drive.google.com/open?id=".$mirror->idcopy);
                     }
                 break;
             case 'mirror1':
