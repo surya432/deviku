79a473c80a64830a24310a65487abe84e325906c
surya432 <hadisurya295@gmail.com>
2019-05-27 02:42:17 +0700

    fix count files

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -338,7 +338,7 @@ trait HelperController
     if (preg_match('@https?://(?:[\w\-]+\.)*(?:drive|docs)\.google\.com/(?:(?:folderview|open|uc)\?(?:[\w\-\%]+=[\w\-\%]*&)*id=|(?:folder|file|document|presentation)/d/|spreadsheet/ccc\?(?:[\w\-\%]+=[\w\-\%]*&)*key=)([\w\-]{28,})@i', $urlVideoDrive, $id)) {
       return $this->CheckHeaderCode($id[1]);
     } else {
-      return false;
+      return "error link";
     }
   }
   function GetIdDrive($urlVideoDrive)
@@ -346,7 +346,7 @@ trait HelperController
     if (preg_match('@https?://(?:[\w\-]+\.)*(?:drive|docs)\.google\.com/(?:(?:folderview|open|uc)\?(?:[\w\-\%]+=[\w\-\%]*&)*id=|(?:folder|file|document|presentation)/d/|spreadsheet/ccc\?(?:[\w\-\%]+=[\w\-\%]*&)*key=)([\w\-]{28,})@i', $urlVideoDrive, $id)) {
       return $this->CheckHeaderCode($id[1]);
     } else {
-      return false;
+      return "error link";
     }
   }
   public function copygd($driveId, $folderid, $title, $token)
