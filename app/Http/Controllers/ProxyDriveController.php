345a2cbc43051c98a361aff33c4a57f393ddd04d
surya432 <hadisurya295@gmail.com>
2019-06-01 02:10:22 +0700

    modif bugs ProxyDriveController

--- app/Http/Controllers/ProxyDriveController.php
+++ app/Http/Controllers/ProxyDriveController.php
@@ -15,33 +15,36 @@ class ProxyDriveController extends Controller
     {
         $idDrive = $request->input('id');
         $videoName = $request->input('videoName');
-        $getlinkproxy = $this->viewsource("https://drive01.herokuapp.com/api/googledrive/" . $idDrive . "?token=ndo&videoName=" . $videoName);
+        $getlinkproxy = $this->viewsource("https://drive01.herokuapp.com/api/proxy/" . $idDrive . "?token=ndo&videoName=" . $videoName);
         $result = json_decode($getlinkproxy, true);
-        if (!isset($result['data'])) {
-            $counts = count($parse['data']);
+        if (isset($result['data'])) {
+            $counts = count($result['data']);
             if ($counts == 0) {
-                return response()->json($result['data'], 404);
+                return response()->json($result['reason'], 404);
             } else {
-                foreach ($result['data'] as $a => $b) {
-                    if ($b['resolution'] == 360 && $counts == 3) {
-                        $this->getLinkAndRedirect($b['src']);
+                $parse1=$result['data'];
+                foreach ( $parse1 as $a ) {
+                    if ($a['label'] == '360p' ) {
+                        return Redirect::away($this->getLinkAndRedirect($a['src']));
                     } else {
-                        if ($b['resolution'] == 480 && $counts == 2) {
-                            $this->getLinkAndRedirect($b['src']);
+                        if ($a['label'] == '480p' ) {
+                        return Redirect::away($this->getLinkAndRedirect($a['src']));
                         } else {
-                            return response()->json("Hanya Quality 720p", 404);
+						return Redirect::away("https://www.googleapis.com/drive/v3/files/".$idDrive ."?alt=media&key=AIzaSyARh3GYAD7zg3BFkGzuoqypfrjtt3bJH7M&name=".$videoName."-720p.mp4");
                         }
                     }
                 }
             }
-        }
+        }else{
+			return response()->json($result, 404);
+		}
     }
     function getLinkAndRedirect($links)
     {
         $values = array("rest-drive01.herokuapp.com", "rest-drive03.herokuapp.com", "rest-drive04.herokuapp.com", "rest-drive02.herokuapp.com");
-        $result = preg_replace_callback("/rest-drive01.herokuapp.com/", function () use ($values) {
+        return $result = preg_replace_callback("/rest-drive01.herokuapp.com/", function () use ($values) {
             return $values[array_rand($values)];
         }, $links);
-        return Redirect::away($result);
+        
     }
 }
