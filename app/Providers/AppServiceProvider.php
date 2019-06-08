46d0e4982f3336a47ec385b6702121b4d2f73dbf
surya432 <hadisurya295@gmail.com>
2019-06-08 15:37:42 +0700

    typos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (env('APP_ENV') !== 'local') {
+        if (env('APP_ENV') != 'local') {
             //$url->forceScheme('https');
             \URL::forceScheme('https');
 
