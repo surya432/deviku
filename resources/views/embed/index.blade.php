e75e1f7bfa09474cb5acfa8259167179903c6de1
surya432 <hadisurya295@gmail.com>
2019-06-09 17:15:58 +0700

    asdasd

--- resources/views/embed/index.blade.php
+++ resources/views/embed/index.blade.php
@@ -197,6 +197,9 @@
         }
         $('.btn').click(function() {
             daplayer.remove();
+            $("#myElement").html(
+                '<div class="spinner"><div class="bounce1"></div> <div class="bounce2"></div> <div class="bounce3"></div></div><div id="notif" class="text-center"><p style="color: blue;">Tunggu Sebentar Ya... :D :)</p></div>'
+            );
             $('.btn').removeClass('btn-danger');
             $('.btn').addClass('btn-primary');
             $(this).removeClass('btn-primary').addClass('btn-danger');
