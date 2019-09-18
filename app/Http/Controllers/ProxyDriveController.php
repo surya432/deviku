37a6bd72fa4c28da7e989e933473aee37732aee0
surya432 <hadisurya432>
2019-09-18 10:58:05 +0700

    modif proxy#9

--- app/Http/Controllers/ProxyDriveController.php
+++ app/Http/Controllers/ProxyDriveController.php
@@ -37,7 +37,7 @@ class ProxyDriveController extends Controller
                 }
             }
         }
-        return response()->json($result['reason'], 404);
+        return "https://www.googleapis.com/drive/v3/files/" . $idDrive . "?alt=media&key=AIzaSyARh3GYAD7zg3BFkGzuoqypfrjtt3bJH7M&name=" . $videoName . "-720p.mp4";
     }
     function getLinkAndRedirect($links)
     {
