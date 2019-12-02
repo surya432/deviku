f7f6431890176e7746a50c14de9acba5dc1215e3
Surya Hadi P <hadisurya295@gmail.com>
2019-12-02 22:41:05 +0700

    embed.php

--- public/embed.php
+++ public/embed.php
@@ -12,6 +12,7 @@
 	<script>
 		jwplayer.key = "zGhSOpbt7hbdG53nW3nDZE0vdyyjy0cNdaQNfA==";
 	</script>
+	<?php include_once('popads.php');?>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
