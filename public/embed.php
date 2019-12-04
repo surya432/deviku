8d496ca25b9b4289e76ec7f89381809794c98b00
Surya Hadi P <hadisurya295@gmail.com>
2019-12-04 23:37:25 +0700

    modif jwplayer embed.php

--- public/embed.php
+++ public/embed.php
@@ -6,12 +6,14 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Plyr.io Player -->
-	<link rel="stylesheet" href="https://cdn.plyr.io/3.3.12/plyr.css">
-	<script type="text/javascript" src="/js/jwplayer.js"></script>
+	<!-- <link rel="stylesheet" href="https://cdn.plyr.io/3.3.12/plyr.css">
+	<script type="text/javascript" src="/js/jwplayer.js"></script> -->
+	<script type="text/javascript" src="//content.jwplatform.com/libraries/0P4vdmeO.js"></script>
+
 	<!-- <script type="text/javascript" src="//cdn.jsdelivr.net/jwplayer/5.10/jwplayer.js"></script> -->
-	<script>
+	<!-- <script>
 		jwplayer.key = "zGhSOpbt7hbdG53nW3nDZE0vdyyjy0cNdaQNfA==";
-	</script>
+	</script> -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<?php //include_once('popads.php');?>
